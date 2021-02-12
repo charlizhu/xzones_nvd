e195ab29e70074e275d0a6cd47883a752cb12f1d
Charlie Zhu <charlie.zhu@yaltysoft.com>
2021-02-11 19:05:54 -0800

    adding some comments

--- bicycle_safety/visualizing/choosePoint2.py
+++ bicycle_safety/visualizing/choosePoint2.py
@@ -1,13 +1,20 @@
 # NOTE: This script is currently incomplete, and seems to work so far, though I am more than certain that I missed
 # many edge cases. With the current data it seems to work fine, but that might not always be the case.
 
+# Current problems I can think of:
+# 1. It currently works assuming that there is only 1 car in the vicinity.
+# 2. Because of the above, it does not actually make a decision as to which TID is possibly the car.
+# 3. I think that the script _should_ assume that the car being TID'ed the most is the correct car. It currently does not do that.
+# 4. Eventually it should tell which car is most critical if two cars might be present, for example. Though it can be justified that this is not necessary because you can't fit two cars within the vicinity of interest.
+# 5. The biggest problem is that the current script cannot switch from detecting one car to the next. i.e.: one car passes by, and another approaches.
+
 class choosePoint(object):
 
     def __init__(self):
         self.f = open("bytes2.txt", "r")
         self.tid = []
         self.tidcounts = []
-        self.currenttids = []
+        self.currenttid = None
         self.lx = []
         self.ly = []
         self.sine = math.sin(math.pi/6)
@@ -48,15 +55,18 @@ class choosePoint(object):
             xLoc = self.cosine * xp - self.sine * yp
             yLoc = self.sine * xp + self.cosine * yp
             # ^^ from TI's script.
+            # I'm sure there is a logic error below...
             if yp < 5: # negating any points that are too far.
                 if datapoint[carIndex] not in self.tid:
                     (self.tid).append(datapoint[carIndex])
                     (self.tidcounts).append(1)
+
                     # self.lx = []
                     # self.ly = []
                 else:
                     indexnumber = (self.tid).index(datapoint[carIndex])
                     (self.tidcounts)[indexnumber] = (self.tidcounts)[indexnumber] + 1
+
                 (self.lx).append(xLoc)
                 (self.ly).append(yLoc)
 
