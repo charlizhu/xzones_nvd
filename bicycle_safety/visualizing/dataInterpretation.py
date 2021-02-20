fb6290f204978d9b2b2cadf218c1bd4eb03878f9
Logan_Zhang <zhikaizhangHULK@gmail.com>
2021-02-19 22:04:16 -0800

    comment out unused lines

--- bicycle_safety/visualizing/dataInterpretation.py
+++ bicycle_safety/visualizing/dataInterpretation.py
@@ -91,7 +91,7 @@ class showData(object):
             self.pointsofinterest.append([float(temp[0]), float(temp[1]), float(temp[2]), float(temp[3])])
         self.plotData()
 
-    def point_average(self, curve, ave_point, num_split, velocities):
+    def point_average(self, curve, ave_point, num_split):
         for i in range(0, len(curve)//num_split):
             temp_vel = []
 
@@ -104,6 +104,8 @@ class showData(object):
     #                  If the trajectory goes within a certain "box" relative to the bike at (0,0)
     #                  and if the velocity is not zero, send warning. Should we used accel as well?
     def plotData(self):
+        #memory usage, not used here
+        #start_mem = hpy().heap().size
         plt.figure()
         plt.axis('off')
         axes = plt.gca()
@@ -120,8 +122,10 @@ class showData(object):
         plt.grid()
         # largestX is to track how far forwards a car has gone. If the car moves backwards, that is perhaps unrealistic
         # on the road, and hence it is being treated as noise for now.
-        largestX = (self.pointsofinterest)[0][0]
-        largestY = (self.pointsofinterest)[0][1]
+        #get memory usage
+
+        #largestX = (self.pointsofinterest)[0][0]
+        #largestY = (self.pointsofinterest)[0][1]
         # To store data used for splining.
         count = 0
         curve = []
@@ -129,9 +133,10 @@ class showData(object):
         numPoints = 12
         num_split = 3
         start_flag = 1
-        validpoint = True
-        pastvel = [np.sign(1),np.sign(1)]
+        #validpoint = True
+        #pastvel = [np.sign(1),np.sign(1)]
         for val,x in enumerate(self.pointsofinterest):
+
             #current, peak = tracemalloc.get_traced_memory()
             #print(f"Current memory usage is {current / 10 ** 6}MB; Peak was {peak / 10 ** 6}MB")
             plt.pause(0.05)
@@ -144,14 +149,14 @@ class showData(object):
             velocityvector = 1.5
 
             # For valid points.
-            point_difference = [np.sign((self.pointsofinterest)[val][0] - largestX), np.sign((self.pointsofinterest)[val][0] - largestY)]
+            #point_difference = [np.sign((self.pointsofinterest)[val][0] - largestX), np.sign((self.pointsofinterest)[val][0] - largestY)]
 
             if 1: #val == 0 or (point_difference[0] == pastvel[0] and point_difference[1] == pastvel[1]):
                 if count < numPoints:
                     newXY = self.coord_rotation2D(-30, [x[0],x[1],x[2], x[3]])
                     curve.append([newXY[0],newXY[1], newXY[2],newXY[3]])
-                    velocity.append([x[2], x[3]])
-                    pastvel = [np.sign((self.pointsofinterest)[val][2]), np.sign((self.pointsofinterest)[val][3])]
+                    #velocity.append([x[2], x[3]])
+                    #pastvel = [np.sign((self.pointsofinterest)[val][2]), np.sign((self.pointsofinterest)[val][3])]
                     if start_flag == 1:
                         kf = Kalman(curve[0][0], curve[0][1], curve[-1][2], curve[-1][3], self.ProcessNoise)
                         start_flag -= 1
@@ -162,8 +167,8 @@ class showData(object):
                         kf.update(curve[-1], self.cov, curve)
                         #start_flag -= 1
                     self.xy_data.append([kf._x[0], kf._x[1]])
-                largestX = (self.pointsofinterest)[val][0]
-                largestY = (self.pointsofinterest)[val][1]
+                #largestX = (self.pointsofinterest)[val][0]
+                #largestY = (self.pointsofinterest)[val][1]
 
                 speed=np.sqrt(x[2]**2+x[3]**2)
                 #velocities = plt.plot([x[0],x[0]+x[2]*speed*velocityvector],[x[1],x[1]+x[3]*speed*velocityvector],'b',alpha=0.5)
@@ -172,8 +177,8 @@ class showData(object):
                 # Documentation: https://docs.scipy.org/doc/scipy/reference/tutorial/interpolate.html
                 if count >=numPoints:
                     start_time = time.time()
-                    xvals = []
-                    yvals = []
+                    #xvals = []
+                    #yvals = []
 
                     curve.pop(0)
                     self.xy_data.pop(0)
@@ -183,12 +188,12 @@ class showData(object):
                     #    kf.update(curve[1], self.cov)
                     self.past_path.append(kf._x)
                     #may use velocity for weighted average, right now it is not implemented
-                    velocity.pop(0)
-                    velocity.append([x[2], x[3]])
+                    #velocity.pop(0)
+                    #velocity.append([x[2], x[3]])
                     ave_point = []
                     all_ave_data = []
 
-                    self.point_average(curve, ave_point, num_split, velocity)
+                    self.point_average(curve, ave_point, num_split)
                     #print(ave_point)
                     #self.point_average(curve, all_ave_data, num_split, velocity)
                     plt.plot(ave_point[-1][0], ave_point[-1][1], 'ro', markersize=10)
@@ -229,9 +234,10 @@ class showData(object):
                     #print("predicted x: " + str(predicted_path[98][0]) + "predicted y: " + str(predicted_path[98][1]))
                     #print("actual x: " + str(curve[-1][0]) + "actual y: " + str(curve[-1][1]))
 
-                    #time taken
+                    #time taken & memory used
+                    #end_mem = hpy().heap().size
                     end_time = time.time()
-                    print("time taken is: " + str(end_time - start_time))
+                    print("time taken is: " + str(end_time - start_time) + "; memory usage is: " + str(end_mem-start_mem))
                     plt.plot([i[0] for i in predicted_path], [i[1] for i in predicted_path], 'g', lw=3)
                     plt.plot([i[0] for i in self.past_path], [i[1] for i in self.past_path], 'b', lw=2)
                     plt.gca().add_patch(Rectangle((self.bound_left, -1*self.bound_up/2),
@@ -251,7 +257,7 @@ class showData(object):
                     fily = self.butter_lowpass_filter(yvals, self.cutoff, self.fs, 6)
                     '''
                 count = count + 1 # Housekeeping
-                bike = plt.plot(0,0,"k>",markersize=25)
+                plt.plot(0,0,"k>",markersize=25) #where the bike is
                 plt.ylabel('Distance to your left (meters)')
                 plt.xlabel('Direction of travel (meters)')
 
@@ -283,9 +289,7 @@ if __name__ == "__main__":
     from Kalman_filter import Kalman
     from matplotlib.patches import Rectangle
     import time
-
-    tracemalloc.start()
-
+    from guppy import hpy
 
     # Using IWR1843 data "tm_demo_uart_stream_5_0-40_both_bundary_gating_2020novel"
     logansdata=showData()
